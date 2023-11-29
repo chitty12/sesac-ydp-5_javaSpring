a94b9b321648847046abfed395cec525f0eaa1fa
chitty12 <cty12angel@naver.com>
2023-11-29 16:50:54 +0900

    spring proj

---
+++ sesac/src/main/java/com/example/sesac/SesacApplication.java
@@ -0,0 +1,13 @@
+package com.example.sesac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesacApplication.class, args);
+	}
+
+}
