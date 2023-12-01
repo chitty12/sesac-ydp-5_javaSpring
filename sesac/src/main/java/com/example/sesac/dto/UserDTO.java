fd3e8ca54bcaa0481c8960dbb7fab1c4edf31956
chitty12 <cty12angel@naver.com>
2023-12-01 17:51:27 +0900

    study: DTO, VO

---
+++ sesac/src/main/java/com/example/sesac/dto/UserDTO.java
@@ -0,0 +1,43 @@
+package com.example.sesac.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+//    get - vo = null (modelattribute = setter 함수를 실행)
+//    post - vo = null
+//    post - vo - requestbody = 오류
+    
+    
+//    @Getter
+//    @Setter
+    private String name;
+//
+//    @Getter
+//    @Setter
+    private String age;
+
+
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(String age) {
+//        this.age = age;
+//    }
+
+
+
+}
---
+++ sesac/src/main/java/com/example/sesac/vo/UserVo.java
@@ -0,0 +1,15 @@
+package com.example.sesac.vo;
+
+import lombok.Getter;
+
+@Getter
+public class UserVo {
+    private String name, age, gender;
+    private String[] interest ;
+    private Integer year, month, day;
+
+
+
+
+
+}
